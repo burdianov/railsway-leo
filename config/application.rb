1d93244e813ee53c75cc18f1e9e86923970f61e7
Leo Burdianov <leo.burdianov@gmail.com>
2016-04-04 00:06:33 +0400

    Configure console to use Pry over IRB

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,10 @@ module Railsway
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    console do
+      require 'pry'
+      config.console = Pry
+    end
   end
 end
